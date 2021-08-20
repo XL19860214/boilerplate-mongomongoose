d40c93def346ceabe5eff0b1d79507f762170845
Xuwei Li <72472538+XL19860214@users.noreply.github.com>
2021-08-20 16:18:50 +0000

    Fulfill https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/chain-search-query-helpers-to-narrow-search-results

--- myApp.js
+++ myApp.js
@@ -82,7 +82,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({
+    favoriteFoods: foodToSearch
+  })
+  .sort('name')
+  .limit(2)
+  .select(['name', 'favoriteFoods'])
+  .exec(done);
+
 };
 
 /** **Well Done !!**
